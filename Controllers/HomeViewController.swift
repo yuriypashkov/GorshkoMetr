7bb167ec964a021a2dc3ff3fd8caff66082dcd12
yuriypashkov <57865915+yuriypashkov@users.noreply.github.com>
2020-07-23 14:47:08 +0300

    commit
    Восстановление проекта, все сломалось почему-то в какой-то момент

--- Controllers/HomeViewController.swift
+++ Controllers/HomeViewController.swift
@@ -1,53 +1,31 @@
-//
-//  MainViewController.swift
-//  GorshkoMetr
-//
-//  Created by Yuriy Pashkov on 6/11/20.
-//  Copyright © 2020 Yuriy Pashkov. All rights reserved.
-//
-
 import UIKit
 
 class HomeViewController: UIViewController {
     
-    var menuViewController: UIViewController!
-    
     let transition = SlideInTransition()
-    //var menuShow = false
-    
-    @IBOutlet weak var backImage: UIImageView!
     
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
         return .portrait
     }
     
     @IBAction func newTapMenu(_ sender: UIButton) {
-        //guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") else { return }
+        guard let menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController") else { return }
         //if #available(iOS 13, *) { menuViewController.modalPresentationStyle = .fullScreen }
         menuViewController.modalPresentationStyle = .overCurrentContext
         menuViewController.transitioningDelegate = self
         present(menuViewController, animated: true, completion: nil)
     }
     
-    @objc func tap(_ sender: AnyObject) {
-        print("TAP VIEW")
-        menuViewController.dismiss(animated: true, completion: nil)
-    }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        menuViewController = storyboard?.instantiateViewController(withIdentifier: "MenuViewController")
-        //попробую отслеживать тап на вьюху
-        let tapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(HomeViewController.tap(_:)))
-        view.isUserInteractionEnabled = true
-        view.addGestureRecognizer(tapGestureRecognizer)
+        //print("done")
+        // Do any additional setup after loading the view.
     }
     
     override var shouldAutorotate: Bool {
         return false
     }
-    
 
 }
 
